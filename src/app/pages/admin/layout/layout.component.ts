016f607bd276acf86ff79668e196f538e09c06c1
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-12-29 23:14:33 +0530

    New function added

--- src/app/pages/admin/layout/layout.component.ts
+++ src/app/pages/admin/layout/layout.component.ts
@@ -1,21 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule,FormsModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-  isSidePanelVisible : boolean = false;
+  isSidePanelVisible: boolean = false;
 
-  openSidePanel(){
-    this.isSidePanelVisible =  true
+  productObj: any = {
+    "id": 0,
+    "title": " ",
+    "description": "",
+    "category": "",
+    "price": 0,
+    "rating": 0,
+    "brand": "",
+    "sku": "",
+    "weight": 0,
+    "warrantyInformation": "",
+    "returnPolicy": "",
+    "thumbnail": "",
   }
 
-  closeSidePanel(){
+
+  openSidePanel() {
+    this.isSidePanelVisible = true
+  }
+
+  closeSidePanel() {
     this.isSidePanelVisible = false
   }
+
+  onSubmit(){}
 }
