d26e8fa82ea509f8ac2969a3ab6cbc0f675d356f
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-12-29 21:53:03 +0530

    New function added

--- src/app/pages/admin/layout/layout.component.ts
+++ src/app/pages/admin/layout/layout.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-  
+  isSidePanelVisible : boolean = false;
+
+  openSidePanel(){
+    this.isSidePanelVisible =  true
+  }
+
+  closeSidePanel(){
+    this.isSidePanelVisible = false
+  }
 }
