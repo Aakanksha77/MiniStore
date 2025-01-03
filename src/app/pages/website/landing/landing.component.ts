cbc8363932b05fd85fde93d9c7cb64ea889acd8e
Aakanksha77 <akanksharajbhar77@gmail.com>
2025-01-03 19:41:19 +0530

    New functions  added

--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-  toggleMenu(){
-    
+  menuActive: boolean = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
   }
 }
