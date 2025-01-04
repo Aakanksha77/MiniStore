a8b804fed55c92a173268df877a969d62953a6d7
Aakanksha77 <akanksharajbhar77@gmail.com>
2025-01-04 21:01:54 +0530

    New functions  added

--- src/app/service/constant/constant.ts
+++ src/app/service/constant/constant.ts
@@ -1,7 +1,8 @@
 export const Constant = {
     API_END_POINT: 'https://dummyjson.com/products/',
     METHODS: {
-        // GET_ALL_PRODUCT: 'products',
+        GET_PRODUCT_BYID: 'products',
         GET_ALL_CATEGORY: 'category-list',
+        GET_PRODUCT_CATEGORY: 'category/',
     }
 }
