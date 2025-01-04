ea9aa1641f2201522726dbba057580993ddb1c49
Aakanksha77 <akanksharajbhar77@gmail.com>
2025-01-05 02:14:41 +0530

    New functions  added

--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -16,6 +16,7 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     this.getproducts()
     this.getAllCategories()
+    this.startAutoSlide()
   }
   
 
@@ -73,9 +74,10 @@ export class LandingComponent implements OnInit {
   }
 
   images: string[] = [
+    'https://media6.ppl-media.com/tr:dpr-2,dpr-2/mediafiles/ecomm/misc/1733820667_3_web.jpg',
     'https://www.maybelline.com/-/media/project/loreal/brand-sites/mny/americas/us/lips-makeup/lip-stick/modules/title-banner/update/1h25_clp_banners_lipstickmakeup_v2.jpg?rev=204a5a1b1f4646bb9475c7b16f2a42d1&cx=0.64&cy=0.84&cw=1320&ch=250&hash=947F0EA2D2A6991CB05160C0C312930E',
     'https://images-static.nykaa.com/uploads/5dc54ba5-f217-4879-ac79-001d29499014.jpg?tr=cm-pad_resize,w-1200',
-    'https://cdn11.bigcommerce.com/s-v9ow9qem2k/images/stencil/original/carousel/201/SKIN-CARE.jpg?c=2',
+    'https://media6.ppl-media.com/tr:dpr-2,dpr-2/mediafiles/ecomm/misc/1733820669_2_web.jpg',
     'https://publish-p33712-e119997.adobeaemcloud.com/content/dam/adityabirlafashionandretailprogram/homepage/fy-2024-2025/nov-24/18nov/plp/a20231118_Chillmodeactivated_Top%20Plp.jpg.transform/i1366x532/image.jpeg'
   ];
   currentIndex: number = 0;
@@ -94,5 +96,12 @@ export class LandingComponent implements OnInit {
     } else {
       this.currentIndex++;
     }
+  }
+
+  autoSlideInterval: any
+  startAutoSlide(): void {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextImage();
+    }, 3000); // Change image every 3 seconds
   }
 }
