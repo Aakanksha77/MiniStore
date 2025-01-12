051cf9017356f3abdff1e0f5198b39b76a4e0bf1
Aakanksha77 <akanksharajbhar77@gmail.com>
2025-01-12 20:26:23 +0530

    new changes in admin layout component

--- src/app/pages/admin/layout/layout.component.ts
+++ src/app/pages/admin/layout/layout.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { ProductsService } from '../../../service/products.service';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-layout',
@@ -10,66 +10,6 @@ import { ProductsService } from '../../../service/products.service';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent implements OnInit{
+export class LayoutComponent{
  
-  router = inject(Router);
-  service = inject(ProductsService);
-
-  catergoriesList: any;
-  existingData: any
-  ngOnInit() {
-    this.getAllCategories()
-    
-    const storedData = localStorage.getItem('apiData');
-    if (storedData) {
-      this.existingData = JSON.parse(storedData);
-    }
-  }
-
-  getAllCategories(){
-    this.service.getAllCategorys().subscribe((result)=>{
-      this.catergoriesList = result
-      console.log(this.catergoriesList);
-      
-    })
-  }
-
-  onCatergoryList(categoryName: string){
-    console.log(categoryName);  
-    this.router.navigateByUrl(`catergories/${categoryName}`);
-  }
-
-  searchResults: any;
-  onSearch(query: string){
-    if (!query.trim()) {
-      this.searchResults = []; // Clear results if query is empty
-      return;
-    }
-  
-    this.searchResults = this.existingData.filter((product: any) => {   
-      // Check if `product.name` exists before calling `toLowerCase`
-      return product.title && product.title.toLowerCase().includes(query.toLowerCase()) || product.description && product.description.toLowerCase().includes(query.toLowerCase());
-    });
-
-    if (this.searchResults.length > 0) {
-      const productId = this.searchResults[0].id; // Get the ID of the first matching product
-      // this.openCard(productId); // Pass the ID to openCard
-      console.log(productId);
-      
-    }
-  
-    console.log('Search Results:', this.searchResults);
-  }
-
-  onAboutUs(){
-    this.router.navigateByUrl('About')
-  }
-
-  onContact(){
-    this.router.navigateByUrl('Contact')
-  }
-
-  onUser(){
-
-  }
 }
